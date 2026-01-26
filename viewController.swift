e999006f962632184155736e07418844f9ebd591
memo figueredo <memof80@gmail.com>
2026-01-26 17:20:40 -0500

    add a variable myName

--- viewController.swift
+++ viewController.swift
@@ -2,12 +2,14 @@ import UIkit
 
 class viewController: UIViewController {
 
-
+    let myName = "daniel"
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     func accountNumber() {
         let number: Int
+
+        print("my Name is: \(myName)")
     }
 }
