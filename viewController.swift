66885ea62108c77b0bfff6bcc306cc5ee181f355
Daniel-DJE-7 <danielfigueredoaranguren@gmail.com>
2026-01-28 16:33:50 -0500

    Update viewController.swift

--- viewController.swift
+++ viewController.swift
@@ -1,18 +1,1 @@
 import UIkit
-
-class viewController: UIViewController {
-
-    let myName = "daniel"
-    let myFavoriteFood = "fruit"
-    let color = "white"
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    func myPersonalInformation() {
-        let number: Int
-
-        print("my Name is: \(myName), \(color), \(myFavoriteFood)")
-    }
-}
